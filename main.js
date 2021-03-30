9eb53c6e612637e9a2fff3ced539dc1b1b02f372
JakeGuy11 <Jake_Guy_11@protonmail.ch>
2021-03-30 18:36:02 -0400

    added extra secret panel

--- main.js
+++ main.js
@@ -6,10 +6,14 @@ var randomImageIndex = Math.round(Math.random() * (7.499 - 1) + 1);
 console.log("Generated index " + randomImageIndex);
 document.body.style.backgroundImage = "url('res/image" + randomImageIndex + ".jpg')";
 
+var specialShown = false;
+
 function toggleHidden(){
 	var schoolPanel = document.getElementById("schoolPanel");
 	var personalPanel = document.getElementById("personalPanel");
 	var hiddenSlide = document.getElementById("hiddenButtonTrack");
+
+	if(specialShown) document.getElementById("secretSlider").click();
 
 	if(schoolPanel.style.display === "none"){
 		schoolPanel.style.display = "block";
@@ -19,9 +23,21 @@ function toggleHidden(){
 		personalPanel.style.display = "block";
 		schoolPanel.style.display = "none";
 		hiddenSlide.style.display = "block";
+
 	}
 }
 
-function showHiddenPanels() {
+function toggleSpecialPanel() {
+	var normalDiv = document.getElementById("normalContainer");
+	var extraDiv = document.getElementById("extraContainer");
 
+	specialShown = !specialShown;
+
+	if(normalDiv.style.display === "none"){
+		normalDiv.style.display = "block";
+		extraDiv.style.display = "none";
+	} else {
+		normalDiv.style.display = "none";
+		extraDiv.style.display = "block";
+	}
 }
